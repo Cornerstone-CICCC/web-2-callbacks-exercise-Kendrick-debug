49e2f014d66aa4fecfe2222c9ecb34cd870733ca
Philip Yaw Boahene <boahenep22@gmail.com>
2024-11-19 22:58:41 -0800

    callback exe complete

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,35 @@
 const fs = require("fs");
 
-fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
+fs.readFile("./firstname.txt", "utf-8", (err, firstname) => {
   // your code here
-})
+  if (err) {
+    console.log(err);
+  } else {
+    fs.readFile("./lastname.txt", "utf-8", (err, lastname) => {
+      // your code here
+      if (err) {
+        console.log(err);
+      } else {
+        fs.readFile("./age.txt", "utf-8", (err, age) => {
+          // your code here
+          if (err) {
+            console.log(err);
+          } else {
+            fs.readFile("./hobbies.txt", "utf-8", (err, hobbies) => {
+              //your code here
+              if (err) {
+                console.log(err);
+              } else {
+                hobbies = JSON.parse(hobbies);
+                console.log(
+                  `${firstname} ${lastname} is ${age} years old and his hobbies are ${hobbies[0]} and ${hobbies[1]}`
+                  // console.log(hobbies[0])
+                );
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+});
